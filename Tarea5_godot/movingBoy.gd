7501ff2a1b4a6821d233404f2457adac6986fa46
dylanJitton <dylanjitton2.0@gmail.com>
2023-08-25 14:14:07 -0400

    Tarea 5 godot Controles

--- Tarea5_godot/movingBoy.gd
+++ Tarea5_godot/movingBoy.gd
@@ -2,44 +2,59 @@ extends Node2D
 
 
 
-@export var speed = 2
-@export var anglespeed=3
+@export var speed = 5
+@export var anglespeed=0.1
 
 var velocity = Vector2(0,0)
 var angle=0
 func _process(delta):
-	velocity=velocity*speed
-	position+=velocity*delta
+	position.x+=velocity.x
+	position.y+=velocity.y
+	rotation+=anglespeed*angle
+	if Input.is_anything_pressed():
+		if Input.is_key_pressed(KEY_UP):
+			_on_button_button_up()
+		if Input.is_key_pressed(KEY_DOWN):
+			_on_button_button_down()
+		if Input.is_key_pressed(KEY_LEFT):
+			_on_button_button_left()
+		if Input.is_key_pressed(KEY_RIGHT):
+			_on_button_button_right()
+		if Input.is_key_pressed(KEY_A):
+			_turn_left()
+		if Input.is_key_pressed(KEY_D):
+			_turn_right()
+	else:
+		_stop_x()
+		_stop_y()
+		_stop_turn()
+	#if Input.is_key_pressed()
 	
-	rotation+=angle*anglespeed*delta
-	
-	
-func update_velocity():
-	velocity = Vector2(0, 0)
+
 
 func _on_button_button_up():
-	update_velocity()
-	velocity.y-=1
+	
+	velocity.y=-speed
 
 func _on_button_button_down():
-	update_velocity()
-	velocity.y+=1
+	
+	velocity.y=speed
 
 func _on_button_button_left():
-	update_velocity()
-	velocity.x-=1
+	
+	velocity.x=-speed
 
 func _on_button_button_right():
-	update_velocity()
-	velocity.x+=1
+	
+	velocity.x=speed
 
 func _stop_x():
-	update_velocity()
-	velocity.x+=0
+	
+	velocity.x=0
 	
 func _stop_y():
-	update_velocity()
-	velocity.y+=0
+	
+	velocity.y=0
 	
 func _turn_left():
 	angle-=1
@@ -49,3 +64,5 @@ func _turn_right():
 	
 func _stop_turn():
 	angle=0
+
+#if Input.is_key
